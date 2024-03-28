2b34a0ee987937484c1633cbe6aad9c168fa5e44
Caio <108128119+DocCaio@users.noreply.github.com>
2024-03-28 14:53:16 -0300

    desktop
    desktop end

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,14 +1,75 @@
 import './Contact.css';
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaInstagramSquare } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
 
 export default function Contact() {
     return (
 
         <footer>
-            <h3>Jadoo.</h3>
+
+            <section  className='contact'>
+            <div>
+              <h3>Jadoo.</h3>
+              <p>Book your trip in minute, get full<br/>
+              Control for much longer.</p>
+            </div>
+
+            <div>
             <h4>Company</h4>
+            <ul>
+                <li>About</li>
+                <li>Careers</li>
+                <li>Mobile</li>
+            </ul>
+            </div>
+
+            <div>
             <h4>Contact</h4>
+            <ul>
+                <li>Help/FAQ</li>
+                <li>Press</li>
+                <li>Affilates</li>
+            </ul>
+                
+            </div> 
+
+            <div>
             <h4>More</h4>
+            <ul>
+                <li>Airlinefees</li>
+                <li>Airline</li>
+                <li>Low fare tips</li>
+            </ul>
+
+            </div>  
+
+            <div>
+            <span>Discover our social medias</span>
+            <ul className='social'>
+                <li><IoLogoWhatsapp /></li>
+                <li><FaInstagramSquare /></li>
+                <li><RiTwitterXLine /></li>
+            </ul>  
+             
+             
+         
+            
+                    
+           
+
+            </div> 
+
+            
+            </section>
+
+            <div className='rights'>
+            
+            <small>All rights</small>
+
+            </div>
         </footer>
+
 
      )
 
