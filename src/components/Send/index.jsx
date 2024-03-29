e26ce33694bf4396a782ec0e9323104436c47509
Caio <108128119+DocCaio@users.noreply.github.com>
2024-03-29 07:53:41 -0300

    up

--- src/components/Send/index.jsx
+++ src/components/Send/index.jsx
@@ -1,8 +1,10 @@
 import  './Send.css';
+import Fly from "./assets/fly.svg";
 export default function Send() {
     return (
         <section className='send'>
             <div className='light'>
+            <img src={Fly} alt='códigos' /> 
                 <h3>
                     Subscribe to get information, latest news and other <br/>
                  interesting offers about Jadoo
